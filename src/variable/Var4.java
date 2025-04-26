fbbf96267254d2aabe0fdc92100cad3836ffa47a
mingming <jimin23623@gmail.com>
2025-04-26 20:42:16 +0900

    study java variables

---
+++ src/variable/Var4.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a = 100;
+        double b = 1.5;
+        boolean c = true; // false
+        char d = 'A'; // 따옴표에 문자 하나만 사용
+        String e = "Hello Java";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
---
+++ src/variable/Var5.java
@@ -0,0 +1,7 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+
+    }
+}
