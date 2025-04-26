000f058dddb5a2a0750078feba565b46eb74ac3a
mingming <jimin23623@gmail.com>
2025-04-26 20:56:49 +0900

    sum variable

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
