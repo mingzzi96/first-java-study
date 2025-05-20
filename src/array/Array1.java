c16e8820f9def7af7fe4e4f3e1b453813f0c7ad4
mingming <jimin23623@gmail.com>
2025-05-20 12:57:56 +0900

    array와for문

---
+++ src/array/Array1.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] students;
+        students = new int[7]; // int가 7개 있다는 뜻
+
+        students[0] = 80;
+        students[1] = 100;
+        students[2] = 10;
+        students[3] = 80;
+        students[4] = 60;
+        students[5] = 100;
+        students[6] = 80;
+
+        for(int i = 0; i < 7; i++){
+            System.out.println("학생" + i + "의 점수 : " + students[i]);
+        }
+
+    }
+}
