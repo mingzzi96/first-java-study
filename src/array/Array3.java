0cbf22da3e2171d06a7624ab7b69bf71f30ec235
mingming <jimin23623@gmail.com>
2025-05-21 12:34:42 +0900

    array 연습 3번째

---
+++ src/array/Array3.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Array3 {
+    public static void main(String[] args) {
+        int[] score = new int[5];
+        int count = score.length;
+        System.out.println(count);
+
+        score[0] = 10;
+        score[1] = 12;
+
+        System.out.println(score[1]);
+    }
+}
